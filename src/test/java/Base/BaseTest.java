a046d4bc53041109bbd4117e3951c36bdf4c4d6f
Helena Papantidis <hpapantidis@gmail.com>
2025-02-14 02:09:03 +0100

    initial commit

---
+++ src/test/java/Base/BaseTest.java
@@ -0,0 +1,37 @@
+package Base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import config.ConfigReader;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String baseURL = ConfigReader.getProperty("baseURL");
+        System.out.println(baseURL);
+        if (baseURL != null) {
+            driver.get(baseURL);
+        }
+        driver.manage().deleteAllCookies();
+    }
+    @AfterMethod
+    public void tearDown() {
+            if (driver != null) {
+                driver.quit();
+            }
+
+        }
+    }
+
+
+
